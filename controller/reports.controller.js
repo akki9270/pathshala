00b9d01e7d669ae6d21b2ba97ff8bd37273a8871
jaydeep4 <jpjp@>
2022-03-26 12:20:32 +0530

    Month Wise data API

--- controller/reports.controller.js
+++ controller/reports.controller.js
@@ -1,4 +1,4 @@
-const { Op, fn, col } = require('sequelize');
+const { Op, fn, col, QueryTypes } = require('sequelize');
 const models = require("../models");
 const moment = require("moment");
 
@@ -8,7 +8,7 @@ exports.GET_DATE_WISE_STUDENT_DATA = async (req, res, next) => {
         let result = await models.Attendence.findAll({
             where: {
                 attendence_date: {
-                    [Op.gte]: moment(date).subtract(1, 'days').startOf('day').toDate(),
+                    [Op.gte]: moment(date).startOf('day').toDate(),
                     [Op.lt]: moment(date).endOf('day').toDate()
                 }
             },
@@ -25,7 +25,14 @@ exports.GET_DATE_WISE_STUDENT_DATA = async (req, res, next) => {
 }
 
 exports.GET_MONTH_WISE_STUDENT_DATA = async (req, res, next) => {
-
+    let startDate = req.query.startDate;
+    let endDate = req.query.endDate;
+    try {
+        let monthData = await models.sequelize.query(`SELECT  DATE(attendence_date) as Date, count(DATE(attendence_date)) as count  from attendence  where attendence_date between ` + startDate + ` and ` + endDate + ` group by DATE(attendence_date);`, {type: QueryTypes.SELECT});
+        return res.status(200).send({monthData: monthData});
+    } catch (e) {
+        return res.status(500).send(e);
+    }
 }
 
 exports.GET_SUTRA_WISE_STUDENT_DATA = async (req, res, next) => {
