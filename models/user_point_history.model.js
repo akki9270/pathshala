fc1e7ebbc72de7f44f967d5cf5e07c4c52a36272
jaydeep4 <jpjp@>
2022-04-07 11:43:58 +0530

    User point modal

---
+++ models/user_point_history.model.js
@@ -0,0 +1,28 @@
+const User = require('./user.model');
+module.exports = function (Sequelize, Types) {
+    let UserPoint = Sequelize.define(
+        "UserPointHistory",
+        {
+            id: { type: Types.INTEGER, autoIncrement: true, primaryKey: true },
+            point: { type: Types.INTEGER },
+            description: { type: Types.STRING, allowNull: false },
+            isPointAdded: { type: Types.TINYINT, defaultValue: '0' }
+        },{
+            paranoid: true,
+            freezeTableName: true,
+            tableName: "user_point_history",
+            modelName: "UserPointHistory"
+        });
+    UserPoint.belongsTo(User(Sequelize, Types), {
+        foreignKey: 'added_by',
+        targetKey: 'id',
+        as: 'Teacher'
+    });
+
+    UserPoint.belongsTo(User(Sequelize, Types), {
+        foreignKey: 'user_id',
+        targetKey: 'id',
+        as: 'Student'
+    });
+    return UserPoint;
+}
