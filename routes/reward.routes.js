9f6182f838e101d81d41d0ee5844e4a1b46967bc
Jaydeep <ptljaydeep4@gmail.com>
2022-04-15 18:56:17 +0530

    - deleteReward route updated

--- routes/reward.routes.js
+++ routes/reward.routes.js
@@ -1,11 +1,12 @@
 const Router = require("express-promise-router");
 const router = new Router();
 
-const { GET_ALL_REWARD, CREATE_REWARD, GET_REWARD_BY_ID } = require('../controller/reward.controller');
+const { GET_ALL_REWARD, CREATE_REWARD, GET_REWARD_BY_ID, DELETE_REWARD } = require('../controller/reward.controller');
 
 router.get('/getAllReward', GET_ALL_REWARD);
 router.post('/createReward', CREATE_REWARD);
 router.post('/updateReward/:id', CREATE_REWARD);
+router.delete('/deleteReward/:id', DELETE_REWARD);
 router.get('/getRewardById/:id', GET_REWARD_BY_ID);
 
 module.exports = router;
