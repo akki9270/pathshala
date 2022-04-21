e8120f35bacbbc534ceecdff0eb5523aa4e56f09
Jaydeep <ptljaydeep4@gmail.com>
2022-04-21 12:31:10 +0530

    - bookReward API bug fixing

--- controller/reward_booked_by.controller.js
+++ controller/reward_booked_by.controller.js
@@ -1,21 +1,21 @@
-const {getAllBookedReward, bookReward} = require('../service/reward_book_by.service');
+const { getAllBookedReward, bookReward } = require('../service/reward_book_by.service');
 
 exports.GET_ALL_BOOKED_REWARD = async (req, res, next) => {
     try {
         let result = await getAllBookedReward();
-        return res.status(200).send({data: result});
+        return res.status(200).send({ data: result });
     } catch (e) {
         return res.status(500).send(e);
     }
 };
 
 exports.BOOK_REWARD = async (req, res, next) => {
-    const {reward_id, user_id} = req.query;
+    const { reward_id, user_id } = req.body;
     try {
         let result = await bookReward({
             reward_id, user_id
         });
-        return res.status(200).send({data: result});
+        return res.status(200).send({ data: result });
     } catch (e) {
         return res.status(500).send(e);
     }
--- service/reward_book_by.service.js
+++ service/reward_book_by.service.js
@@ -4,24 +4,24 @@ async function getAllBookedReward() {
     try {
         let result = await models.RewardBookedBy.findAll({
             include: [
-                {model: models.User, as: 'Student'},
-                {model: models.Reward, as: 'Reward'}
+                { model: models.User, as: 'Student' },
+                { model: models.Reward, as: 'Reward' }
             ],
             order: [['start_date', 'DESC']]
         });
-        return {data: result}
+        return { data: result }
     } catch (e) {
-        return {isError: true, e: e};
+        return { isError: true, e: e };
     }
 }
 
 async function bookReward(rewardBooked) {
     try {
-        let result = await models.RewardBookedBy.create({rewardBooked});
-        return {data: result}
+        let result = await models.RewardBookedBy.create(rewardBooked);
+        return { data: result }
     } catch (e) {
         console.log(e)
-        return {isError: true, e: e};
+        return { isError: true, e: e };
     }
 }
 
